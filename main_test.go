03b5fea5970f209b6face1c02e4269d4e4b119dc
hero <ipioneerbackup@gmail.com>
2023-03-14 14:36:56 +0200

    Added unit tests for SET and GET routes

--- main_test.go
+++ main_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/libraryAPI/entity"
 	"github.com/libraryAPI/repository"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,6 +16,7 @@ import (
 func TestGetBooksEndpoint(t *testing.T) {
 	router := SetUpRouter()
 	books = repository.NewLibrary()
+
 	router.GET("/books", getBooks)
 	request, _ := http.NewRequest("GET", "/books", nil)
 	writer := httptest.NewRecorder()
@@ -23,6 +26,40 @@ func TestGetBooksEndpoint(t *testing.T) {
 	json.Unmarshal(writer.Body.Bytes(), &bookList)
 	assert.Equal(t, http.StatusOK, writer.Code)
 	assert.NotEmpty(t, bookList)
+
+}
+
+func TestSetBooksEndpoint(t *testing.T) {
+	router := SetUpRouter()
+
+	router.POST("/books", setBooks)
+	mockResponse := "\"Books were set\""
+	books := []entity.Book{
+		{
+			Name:   "Rage",
+			Author: "Stephen King",
+			Year:   1900,
+		},
+		{
+			Name:   "Philosopher's Stone",
+			Author: "J. K. Rowling",
+			Year:   1900,
+		},
+		{
+			Name:   "All Quiet on the Western Front",
+			Author: "Erich Maria Remarque",
+			Year:   1900,
+		},
+	}
+
+	jsonValue, _ := json.Marshal(books)
+	req, _ := http.NewRequest("POST", "/books", bytes.NewBuffer(jsonValue))
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
 
 }
 
